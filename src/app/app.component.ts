13c93b48dc4b2d5729bb3fc299ddf3eb34896c86
Lucile Jones <jones.lucile@gmail.com>
2023-10-31 17:20:29 -0600

    project setup and through post and get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'academind-http-requests';
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    // console.log(postData);
+    this.http.post(
+      'https://academind-http-requests-default-rtdb.firebaseio.com/posts.json',
+      postData
+      ).subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('https://academind-http-requests-default-rtdb.firebaseio.com/posts.json').subscribe(posts => {
+      console.log(posts);
+    });
+  }
 }
+
